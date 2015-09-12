daea6abd9321ce25db361846ad44df80945ff3dd
mkruk <mkruk@humedica.com>
2015-09-12 16:52:50 -0400

    adding

---
+++ hw1/lists.py
@@ -0,0 +1,18 @@
+list1 = [1, 1, 2, 3, 5, 8]
+print(list1)
+
+print(sum(list1))
+print(max(list1))
+print(min(list1))
+print(2 in list1)
+print("Misha" in list1)
+
+print(list1[0])
+print(list1[-1])
+print(list1[3:5])
+print(list1[2:])
+
+list1.reverse()
+print(list1)
+
+
